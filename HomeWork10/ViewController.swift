13f296febbdccfb88bdb72b1aebd8ac5a63a3d9c
kunduz <kunduznbkva@gmail.com>
2022-06-09 01:51:24 +0600

    Delete unnecessary comments in code

--- HomeWork10/ViewController.swift
+++ HomeWork10/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  HomeWork10
-//
-//  Created by Kunduz on 09.06.2022.
-//
+
 
 import UIKit
 
@@ -11,7 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
